090151db8eb575e93200feb7a1e781778a7a32b6
Cosmin Tupangiu <cosmin@redhat.com>
2023-04-21 09:59:33 +0200

    add repo aliases
    Signed-off-by: Cosmin Tupangiu <cosmin@redhat.com>

--- internal/repo/alias.go
+++ internal/repo/alias.go
@@ -1,10 +1,29 @@
 package repo
 
-import "github.com/tupyy/tinyedge-controller/internal/repo/postgres"
+import (
+	"github.com/tupyy/tinyedge-controller/internal/repo/cache"
+	"github.com/tupyy/tinyedge-controller/internal/repo/git"
+	"github.com/tupyy/tinyedge-controller/internal/repo/postgres"
+	"github.com/tupyy/tinyedge-controller/internal/repo/vault/certificate"
+	"github.com/tupyy/tinyedge-controller/internal/repo/vault/secret"
+)
 
-type DeviceRepo = postgres.DeviceRepo
+type (
+	Device      postgres.DeviceRepo
+	Manifest    postgres.ManifestRepository
+	Repository  postgres.Repository
+	Git         git.GitRepo
+	Certificate certificate.CertficateRepo
+	Secret      secret.Repository
+	MemCache    cache.MemCacheRepo
+)
 
 var (
-	NewDeviceRepo           = postgres.NewDeviceRepo
-	NewDeviceRepoWithReader = postgres.NewDeviceRepoWithReader
+	NewDeviceRepo  = postgres.NewDeviceRepo
+	NewManifest    = postgres.NewManifestRepository
+	NewRepository  = postgres.NewRepository
+	NewGit         = git.New
+	NewCertificate = certificate.New
+	NewSecret      = secret.New
+	NewMemCache    = cache.NewCacheRepo
 )
--- internal/repo/vault/secret/repo.go
+++ internal/repo/vault/secret/repo.go
@@ -1,4 +1,4 @@
-package vault
+package secret
 
 import (
 	"bytes"
